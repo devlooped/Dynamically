de73104e31ef548c038c24d3d7724e4bc0669b25
Daniel Cazzulino <daniel@cazzulino.com>
2023-02-17 19:24:57 -0300

    💜 Add SponsorLink
    See https://www.cazzulino.com/sponsorlink.html

---
+++ src/Dynamically/SponsorLinker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Devlooped.Dynamically;
+
+[Generator]
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+class SponsorLinker : SponsorLink
+{
+    public SponsorLinker() : base(SponsorLinkSettings.Create(
+        "devlooped", "Dynamically",
+        packageId: ThisAssembly.Project.PackageId,
+        version: new Version(ThisAssembly.Info.Version).ToString(2)
+#if DEBUG
+        , quietDays: 0
+#endif
+        ))
+    { }
+}
